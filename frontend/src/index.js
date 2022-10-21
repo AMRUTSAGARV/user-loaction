03aa150b85cea32673ffb97a9c070014887cabeb
Amrut Sagar <amrutsagar06@gmail.com>
2022-10-21 15:15:00 +0530

    ui components

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
-
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import "bootstrap/dist/css/bootstrap.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <React.Fragment>
     <App />
-  </BrowserRouter>
+  </React.Fragment>
 );
 
 reportWebVitals();
