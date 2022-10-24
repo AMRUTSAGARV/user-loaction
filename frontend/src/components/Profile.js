bdf30be3c2a1dd72774236d4e4d1791fe339d37f
Amrut Sagar <amrutsagar06@gmail.com>
2022-10-24 23:24:13 +0530

    branch handler

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import Select from "react-select";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import { useParams, useNavigate } from "react-router-dom";
@@ -10,6 +11,17 @@ const Profile = ({ isBranch, setIsBranch, isAddBranch, setisAddBranch }) => {
   const [state, setState] = useState({ branchName: "" });
 
   const { id } = useParams();
+  const getBranch = () => {
+    branchService.getBranchById(id);
+  };
+
+  useEffect(() => {
+    getBranch();
+  }, [isAddBranch, isBranch]);
+
+  useEffect(() => {
+    getBranch();
+  }, []);
 
   const currentUser = AuthService.getCurrentUser();
   const [show, setShow] = useState(false);
@@ -27,6 +39,26 @@ const Profile = ({ isBranch, setIsBranch, isAddBranch, setisAddBranch }) => {
   };
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  var branchList = [
+    {
+      value: 1,
+      label: "Branch 1",
+    },
+    {
+      value: 2,
+      label: "Branch 2",
+    },
+    {
+      value: 3,
+      label: "Branch 3",
+    },
+  ];
+
+  const [result, branchValue] = useState(branchList.value);
+  const branchHandler = (e) => {
+    branchValue(e.value);
+  };
 
   return (
     <div>
@@ -46,7 +78,7 @@ const Profile = ({ isBranch, setIsBranch, isAddBranch, setisAddBranch }) => {
           >
             Dont have a branch? Click here to select branch
           </button>
-          <Modal show={show} onHide={handleClose}>
+          {/* <Modal show={show} onHide={handleClose}>
             <Modal.Header closeButton>
               <Modal.Title>Select Branch</Modal.Title>
             </Modal.Header>
@@ -65,11 +97,19 @@ const Profile = ({ isBranch, setIsBranch, isAddBranch, setisAddBranch }) => {
               <Button variant="secondary" onClick={handleClose}>
                 Close
               </Button>
-              <Button variant="primary" onClick={handleClose}>
+              <Button
+                variant="primary"
+                onClick={handleClose}
+                onSubmit={(e) => {
+                  handleSubmit(e);
+                }}
+              >
                 Submit
               </Button>
             </Modal.Footer>
-          </Modal>
+          </Modal> */}
+
+          <Select options={branchList} onChange={branchHandler} />
         </div>
       </div>
     </div>
